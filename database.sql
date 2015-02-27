23b67d6e2ec7f108a9eb6403caa46c8dd473fb8d
Catlord <benoit_carde@yahoo.fr>
2015-02-27 10:11:51 +0100

    Database
    database

---
+++ database.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 27 Février 2015 à 10:07
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `database`
+--
+
+CREATE TABLE IF NOT EXISTS `database` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `date` int(10) NOT NULL,
+  `actif` int(10) NOT NULL,
+  `champ1` varchar(255) NOT NULL,
+  `champ2` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
