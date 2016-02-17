f6da9ad5e373aa5777dddefccf4ce814c49348e9
Catlord <contact@nantes-web-creation.com>
2016-02-17 18:54:18 +0100

    bd.php - php 5.5
    update

--- bd.php
+++ bd.php
@@ -1,9 +1,11 @@
 <?php
 
+
 class bd {
 	private $bd;
 	private $host,$login,$pass,$base;
 	public $lock;
+	
 
 	public function __construct($host,$login,$pass,$base) {
 		$this->bd=NULL;
@@ -13,74 +15,56 @@ class bd {
 		$this->base=$base;
 	}
 
-	public function __destruct() {
-		if($this->bd) $this->close();
-	}
-
 	public function init() {
 		if($this->bd) return;
-
-		if(!$this->bd=mysql_connect($this->host,$this->login,$this->pass)) {
-			$errstr='mysql_connect ('.mysql_errno().') :'."\n".mysql_error();
-			trigger_error($errstr,E_USER_ERROR);
-			header_html_500($errstr);
-		}
-		if(!mysql_select_db($this->base,$this->bd)) {
-			$errstr='mysql_select_db ('.mysql_errno().') : '.$this->base."\n".mysql_error();
-			trigger_error($errstr,E_USER_ERROR);
-			$this->close($bd);
-			header_html_500($errstr);
-		}
-		if(!mysql_query('SET NAMES "utf8"')) {
-			$errstr='mysql_query utf8 ('.mysql_errno().') : '.$this->base."\n".mysql_error();
-			trigger_error($errstr,E_USER_ERROR);
-			$this->close($bd);
-			header_html_500($errstr);
+		$options = array(
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+		);
+		if(DEBUG)
+			$options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+		$dsn = "mysql:host=".$this->host.";dbname=".$this->base;
+		try{
+		$this->bd = new PDO($dsn,$this->login,$this->pass,$options);
+		}catch(PDOException $e){
+			echo 'Connexion échouée : ' . $e->getMessage();
 		}
 	}
 
-	// give bd (NULL si non connecté)
+	// Donne bd (NULL si non connecté)
 	public function bd() { return($this->bd); }
 
-	// Closing database 
+	// ferme le lien avec la bd
 	public function close() {
-		if(!mysql_close($this->bd)) {
-			$errstr='mysql_close ('.mysql_errno().') :'."\n".mysql_error();
-			trigger_error($errstr,E_USER_ERROR);
-		}
 		$this->bd=NULL;
 	}
 
-	// Secure sql request element 
-	public function secure($val) { return mysql_real_escape_string($val,$this->bd); }
 
-	public function sql($sql) { // requesting
-		if($res=@mysql_query($sql,$this->bd)) return $res;
-
-		if($errno=mysql_errno($this->bd)) {
-			$errstr='mysql_query ('.mysql_errno($this->bd).') :'."\n".mysql_error($this->bd)."\n\n".$sql;
-			trigger_error($errstr,E_USER_ERROR);
-		}
+	//Fait une requete sql et retourne les resultat sous forme de tableau
+	public function select($sql) { // soumission d'une requete
+		$req = $this->bd->query($sql);
+		$res = $req->fetchAll();
+		$req->closeCursor();
+		return $res;
 	}
-
-	// Nb lines affected 
-	public function affected() { return mysql_affected_rows($this->bd); }
-
-	// Nb results from a request
-	public function nb($res) { return mysql_num_rows($res); }
-
-	// obtain the next result
-	public function result($result) { return mysql_fetch_assoc($result); }
-
-	// obtain the last id auto-i with an insert 
-	public function lastid() { return mysql_insert_id($this->bd); }
-
-	// Emptying table
-	public function vide($table) { return $this->sql('TRUNCATE TABLE '.$table); }
-
-	// freeing memory mysql results once used 
-	public function free(&$res) { return mysql_free_result($res); }
-
+	
+	//Fait une requete select securisée
+	public function prepareSelect($sql) { // soumission d'une requete
+		$req = $this->bd->prepare($sql);
+		$res = $req->fetchAll();
+		$req->closeCursor();
+		return $res;
+	}
+	
+		//Fait une requete select securisée retourne false si la req echoue
+	public function prepareExec($sql,$array) { // soumission d'une requete
+		$req = $this->bd->prepare($sql);
+		$req->execute($array);
+		return $req;
+	}
+	
+	//faire des requetes qui ne retourne pas de ligne (UPDATE / INSERT / DELETE )
+	public function exec($sql) { // soumission d'une requete
+		$req = $this->bd->exec($sql);
+		return $req;
+	}
 }
-
-?>
